d5f29889b058b17bde393a012b94c0ea5414c593
Henrique Butzke Kotz <henriquebkotz@gmail.com>
2022-03-20 18:11:08 -0300

    fix random deck

--- src/components/Deck/Deck.js
+++ src/components/Deck/Deck.js
@@ -3,48 +3,8 @@ import Footer from './Footer';
 
 import { useState } from 'react';
 
-export default function Deck() {
-    const deck = [
-        {
-            question: 'O que é JSX?',
-            answer: 'Uma extensão da linguagem do JavaScript'
-        },
-        {
-            question: 'O React é ___',
-            answer: 'uma biblioteca JavaScript para construção de interfaces'
-        },
-        {
-            question: 'Componentes devem iniciar com ___',
-            answer: 'letra maiúscula'
-        },
-        {
-            question: 'Podemos colocar ___ dentro do JSX',
-            answer: 'expressões'
-        },
-        {
-            question: 'O ReactDOM nos ajuda ___',
-            answer: 'interagindo com o DOM para colocar componentes React no mesmo'
-        },
-        {
-            question: 'Usamos npm para ___',
-            answer: 'gerenciar os pacotes necessários e suas dependências'
-        },
-        {
-            question: 'Usamos props para ___',
-            answer: 'passar diferentes informações para componentes'
-        },
-        {
-            question: 'Usamos estado (state) para ___',
-            answer: 'dizer para o React quais informações, quando atualizadas, dever renderizar a tela novamente'
-        }
-    ];
-
+export default function Deck({ deck }) {
     const [answerSequence, setAnswerSequence] = useState([]);
-    deck.sort(comparador);
-
-    function comparador() { 
-        return Math.random() - 0.5; 
-    }
 
     return (
     <>
